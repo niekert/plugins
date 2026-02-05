4e5bc31d15fc9a3d33c759720264c44d4496e30c
Niek <niek@framer.com>
2026-02-05 16:44:29 +0100

    run fix-biome

--- packages/plugin-tools/src/lib.ts
+++ packages/plugin-tools/src/lib.ts
@@ -1,4 +1,4 @@
 // Library exports for programmatic use
 
-export { runBuildScript, zipDistFolder } from "./build-and-zip"
 export type { BuildAndZipOptions, BuildAndZipResult } from "./build-and-zip"
+export { runBuildScript, zipDistFolder } from "./build-and-zip"
